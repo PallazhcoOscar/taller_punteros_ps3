7b1ebe66f81726d46502333984f58ffdd262f89a
oscarP <oepallaz@espol.edu.ec>
2017-10-30 15:34:45 -0500

    modificaiion

--- palindromo.c
+++ palindromo.c
@@ -10,7 +10,6 @@ int main (){
 	int lens1 = strlen(&s1);
 	printf("Imprimir s1:  %s, tiene %d letras\n", s1, lens1);
 	int pal = palindromo(s1);
-	printf("val de la funcion palindromo: %d \n", pal);
 	if(pal)
 		printf("Palindromo\n");
 	else
@@ -32,9 +31,7 @@ int palindromo(char texto[]){
 	int tam;
 	int i,j;	
 	j = strlen(&texto) - 3;
-	printf("tamaño : %d\n", j);
 	for(i = 0; i < j/2; i++, j--){
-		printf(" i: %c , j: %c\n ", texto[i], texto[j]);
 		if(texto[i] != texto[j])
 			return 0;
 	}
